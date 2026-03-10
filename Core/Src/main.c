c7b80c049ed98c2132c1f533becfadd7d77790bd
ndthenhan@gmail.com <ndthenhan@gmail.com>
2026-03-10 21:19:19 +0100

    all commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -112,21 +112,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-  //test
-  HAL_Delay(1000);
-  HAL_GPIO_WritePin(GPIOB, LD1_Pin, GPIO_PIN_RESET);
-  HAL_Delay(1000);
-  HAL_GPIO_WritePin(GPIOB, LD1_Pin, GPIO_PIN_SET);
-  HAL_Delay(1000);
-  HAL_GPIO_WritePin(GPIOB, LD2_Pin, GPIO_PIN_RESET);
-  HAL_Delay(1000);
-  HAL_GPIO_WritePin(GPIOB, LD2_Pin, GPIO_PIN_SET);
-  HAL_Delay(1000);
-  HAL_GPIO_WritePin(GPIOB, LD3_Pin, GPIO_PIN_RESET);
-  HAL_Delay(1000);
-  HAL_GPIO_WritePin(GPIOB, LD3_Pin, GPIO_PIN_SET);
-  
-  /* USER CODE BEGIN 3 */
+
+    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
